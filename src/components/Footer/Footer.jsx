4d066353b3c160ad3d46ea64c1cf2c85b3247354
Karol <karolrds01@gmail.com>
2023-03-17 09:22:33 -0400

    icon footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import icon from '/assets/marca_registrada.svg'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-        <img src="../../public/assets/marca_registrada.svg" alt="Marca registrada" />
+        <img src={icon} alt="Marca registrada" />
         <p>Desenvolvido por Karol e Alura</p>
     </footer>
   )
